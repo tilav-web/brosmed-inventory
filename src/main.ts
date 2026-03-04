d24b9b55127cd856ada8786aaa5f0fe51dc4e882
Tilav <shavqiddintilovov0@gmail.com>
2026-03-04 12:36:32 +0500

    brosmed inventory init project

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap()
+  .then(() => {
+    console.log('Application is running on port 3000');
+  })
+  .catch((error) => {
+    console.error('Error starting the application:', error);
+  });
