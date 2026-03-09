3c9d057f53f05eac6d31843d1fb7814d21c0d60f
Tilav <shavqiddintilovov0@gmail.com>
2026-03-09 13:02:32 +0500

    .

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
