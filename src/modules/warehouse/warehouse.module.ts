13aa2d5a629b7bd8f146746807ea9386705703d1
Tilav <shavqiddintilovov0@gmail.com>
2026-03-23 13:57:19 +0500

    .

--- src/modules/warehouse/warehouse.module.ts
+++ src/modules/warehouse/warehouse.module.ts
@@ -3,12 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../product/entities/product.entity';
 import { ProductBatch } from '../product/entities/product-batch.entity';
 import { User } from '../user/entities/user.entity';
+import { ExpenseItem } from '../expense/entities/expense-item.entity';
 import { WarehouseController } from './controllers/warehouse.controller';
 import { Warehouse } from './entities/warehouse.entity';
 import { WarehouseService } from './services/warehouse.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Warehouse, User, Product, ProductBatch])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Warehouse,
+      User,
+      Product,
+      ProductBatch,
+      ExpenseItem,
+    ]),
+  ],
   controllers: [WarehouseController],
   providers: [WarehouseService],
   exports: [WarehouseService],
